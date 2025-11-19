ae557cf4ee53c6479e3d73997e8977f8a6174d10
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-11-19 19:13:25 +0530

    Final Push

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React, { useState } from "react";
+import "./app.css";
+const App = () => {
+  const [value, setValue] = useState(0);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <article>
+        <header>Counter App</header>
+        <section>
+          <h1>{value}</h1>
+          <div className="buttons">
+            <button className="btn" onClick={()=>setValue(value-1)} disabled={value===0}>-</button>
+            <button className="btn"onClick={()=>setValue(value+1)}>+</button>
+          </div>
+        </section>
+        <footer>&copy; All Rights Reserved By Vansh</footer>
+      </article>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
